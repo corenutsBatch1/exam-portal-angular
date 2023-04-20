def5607d39df9d6ad18f04c0fd3635483c1787de
sambanna07 <115611654+sambanna07@users.noreply.github.com>
2023-04-20 09:02:39 +0530

    validation

--- src/app/components/loginmodal/loginmodal.component.ts
+++ src/app/components/loginmodal/loginmodal.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from '../../model/model/User';
 import { LoginserviceService } from './loginservice.service';
+import { FormGroup,Validators,FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-loginmodal',
@@ -10,14 +11,37 @@ import { LoginserviceService } from './loginservice.service';
   styleUrls: ['./loginmodal.component.css'],
 })
 export class LoginmodalComponent {
+    //Form variables
+loginForm:any = FormGroup;
+submitted = false;
+
   userLogin:User = new User();
   validUser?:User;
   constructor(private http:HttpClient,private router: Router,
-     private loginservice: LoginserviceService,
+     private loginservice: LoginserviceService,private formBuilder:FormBuilder
      ) {}
 
+     //Add user form actions
+get f()
+{ return this.loginForm.controls; }
+
+ngOnInit() {
+  //Add User form validations
+  this.loginForm = this.formBuilder.group({
+  lemail: ['', [Validators.required, Validators.email]],
+  lpassword: ['', [Validators.required,Validators.minLength(6)]]
+  });
+}
+
   login(user: User) {
+  this.submitted = true;
     console.log(user);
+    if (this.loginForm.invalid) {
+      console.log("false");
+      return;
+    }
+    if(this.submitted)
+     {
     this.http
       .post(`http://localhost:8088/api/loginUser`, user)
       .subscribe((data) => {
@@ -34,7 +58,8 @@ export class LoginmodalComponent {
           }
         } else {
           sweetAlert("Invalid Credentials", "Register and try again", "error");
-        }
-      });
-  }
+        }
+     });
+    }
+   }
 }
--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,21 +1,84 @@
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import swal from 'sweetalert';
 import { User } from '../../model/model/User';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
+
 export class SignupComponent {
+  signUpForm:any=FormGroup;
+  submitted = false;
+
   data:any | undefined;
   userLogin:User = new User();
-  constructor(private http:HttpClient, private router : Router){}
+  constructor(private http:HttpClient, private router : Router,
+    private formBuilder:FormBuilder){}
 
-  registerNewUser(newUser:User){
-    console.log(newUser)
+  get f()
+  {
+   return this.signUpForm.controls;
+  }
+
+ngOnInit(){
+  //Add User form validations
+  this.signUpForm = this.formBuilder.group({
+  sname:
+  [
+    '',
+   [
+    Validators.required,
+    Validators.minLength(3),
+    Validators.maxLength(30),
+    Validators.pattern('[a-zA-Z]*')
+   ]
+ ],
+  semail:
+  [
+    '',
+    [
+      Validators.required,
+      Validators.email
+    ]
+  ],
+  spassword:
+  [
+    '',
+    [
+      Validators.required,
+      Validators.minLength(6)
+    ]
+  ],
+  sphoneNumber:
+  [
+    '',
+    [
+      Validators.required,
+      Validators.pattern('[0-9]*'),
+      Validators.minLength(10),
+      Validators.maxLength(11)
+    ]
+  ],
+  });
+}
+
+
+
+registerNewUser(newUser:User){
+this.submitted = true;
+console.log(newUser);
+if (this.signUpForm.invalid) {
+  console.log("false");
+  return;
+}
+if(this.submitted)
+{
     this.http.post(`http://localhost:8088/api/registerUser`, newUser).subscribe(data=> {
       if(data !=null){
         swal("Registered successfully", "", "success");
@@ -25,4 +88,5 @@ export class SignupComponent {
       }
     });
   }
+}
 }

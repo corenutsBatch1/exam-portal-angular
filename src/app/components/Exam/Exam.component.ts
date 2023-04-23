44cebe259396d3a40829db801472be41c8aaa7d7
Karthik Rasamsetti <92678697+karthikrasamsetti@users.noreply.github.com>
2023-04-23 16:12:19 +0530

    comit (#29)

--- src/app/components/Exam/Exam.component.ts
+++ src/app/components/Exam/Exam.component.ts
@@ -1,43 +1,9 @@
-// import { Component, OnInit } from '@angular/core';
-
-// @Component({
-//   selector: 'app-Exam',
-//   templateUrl: './Exam.component.html',
-//   styleUrls: ['./Exam.component.css']
-// })
-// export class ExamComponent implements OnInit {
-//   currentQuestion?: number;
-//   constructor() { }
-
-//   ngOnInit() {
-//   }
-
-
-//   showQuestion(questionNumber: number): void {
-//     this.currentQuestion = questionNumber;
-//   }
-
-//   getQuestionText(questionNumber: number): string {
-//     // replace with your own logic to get the question text from a database or other data source
-//     switch (questionNumber) {
-//       case 1:
-//         return 'What is the capital of France?';
-//       case 2:
-//         return 'What is the largest planet in our solar system?';
-//       case 3:
-//         return 'What is the formula for calculating the area of a circle?';
-//       // add more cases here for each question
-//       default:
-//         return '';
-//     }
-//   }
-
-// }
 import { Component, Input } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 import { Question } from 'src/app/model/model/Question';
+import { Subject } from 'src/app/model/model/Subject';
 
 
 
@@ -51,38 +17,53 @@ export class ExamComponent {
   currentQuestion?: Question ;
 
   questions: Question[] = [];
-  // id?:number=this.currentQuestion?.id;
+  subjects: Subject[] =[];
+  uniqueSubjectNames: String[] =[];
 
   constructor(private http: HttpClient,private route:ActivatedRoute) {}
 
   ngOnInit(): void {
-    // this.loadQuestions().subscribe((questions: Question[]) => {
-    //   this.questions = questions;
-    // });
     this.route.params.subscribe(params => {
       this.code = params['code'];
       console.log('Exam code:', this.code);
-      this.loadQuestions().subscribe((questions: Question[]) => {
-          this.questions = questions.sort((a, b) => a.id - b.id);
-          console.log(this.questions)
+      this.loadSubjects().subscribe((subjects: Subject[]) => {
+          this.subjects = subjects;
+          this.uniqueSubjectNames = this.getUniqueSubjectNames(this.subjects)
+          //console.log(this.questions)
         });
     });
-
   }
 
-  loadQuestions(): Observable<Question[]> {
-    return this.http.post<Question[]>(`http://localhost:8089/api/getquestionsBycode/${this.code}`,this.code);
+  getUniqueSubjectNames(subjects: Subject[]): String[] {
+    const uniquesubjects = subjects
+      .map((subjects) => subjects.name)
+      .filter((name) => name !== undefined) as String[];
+    return [...new Set(uniquesubjects)];
+  }
+
+  loadSubjects(): Observable<Subject[]> {
+    return this.http.post<Subject[]>(`http://localhost:8089/api/getsubjectsBycode/${this.code}`,this.code);
+  }
+
+  getQuestionsBySubjectName(subjectName:String):void{
+    this.questions=[];
+    this.subjects.forEach((subject)=>{
+                  if(subject.name==subjectName){
+                    this.loadQuestions(subject.id).subscribe((data)=>{this.questions=this.questions.concat(data)
+                    console.log(data)})
+                  }})
+  }
+
+  loadQuestions(subjectId?: number): Observable<Question[]> {
+    return this.http.get<Question[]>(`http://localhost:8089/api/getallquestions/${subjectId}`);
   }
 
   showQuestion(questionId: number): void {
-    this.loadQuestion(questionId).subscribe((question: Question) => {
-      this.currentQuestion = question;
-      console.log(this.currentQuestion)
-    });
-  }
+    this.questions.forEach((question)=>{
+                            if(question.id==questionId){
+                              this.currentQuestion=question;
+                            }
+    })
 
-  loadQuestion(questionId: number): Observable<Question> {
-    return this.http.get<Question>(`http://localhost:8089/api/getquestionbyid/${questionId}`);
   }
 }
-

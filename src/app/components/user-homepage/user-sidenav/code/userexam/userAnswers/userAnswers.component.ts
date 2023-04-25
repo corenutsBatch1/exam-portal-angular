2572a728b3aff342059cbf24760b025a459e5961
Suresh-130 <129723623+Suresh-130@users.noreply.github.com>
2023-04-25 18:24:36 +0530

    Score (#40)

--- src/app/components/user-homepage/user-sidenav/code/userexam/userAnswers/userAnswers.component.ts
+++ src/app/components/user-homepage/user-sidenav/code/userexam/userAnswers/userAnswers.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Question } from 'src/app/model/model/Question';
+import { useranswer } from 'src/app/model/model/useranswer';
 import { MyserviceService } from 'src/app/model/myservice';
 
 @Component({
@@ -15,7 +16,9 @@ export class UserAnswersComponent implements OnInit {
   uid:any;
   eid:any;
   questions:Question[]=[];
-  
+  score?:Number;
+  userAnswers: useranswer[]=[];
+
   constructor(private http: HttpClient,private route:ActivatedRoute,private service:MyserviceService
     ) { }
 
@@ -31,10 +34,19 @@ export class UserAnswersComponent implements OnInit {
         console.log(this.questions)})
 
   })
+  this.loadScore().subscribe(arg => this.score = arg);
+  this.loadUserAnswers().subscribe((data)=>this.userAnswers=data)
+
 }
   loadQuestions(): Observable<Question[]> {
     return this.http.get<Question[]>(`http://localhost:8089/api/getquestionsBySubjectId/${this.code}`);
   }
 
+  loadScore(): Observable<Number>{
+    return this.http.get<Number>(`http://localhost:8090/api/getScore/${this.uid}/${this.eid}`);
+  }
 
+  loadUserAnswers(): Observable<useranswer[]>{
+    return this.http.get<useranswer[]>(`http://localhost:8089/api/getUserAnswers/${this.uid}/${this.eid}`);
+  }
 }
--- src/app/components/user-homepage/user-sidenav/code/userexam/userexam.component.ts
+++ src/app/components/user-homepage/user-sidenav/code/userexam/userexam.component.ts
@@ -8,51 +8,51 @@ import { useranswer } from 'src/app/model/model/useranswer';
 import { MyserviceService } from 'src/app/model/myservice';
 import { ScheduleExam } from 'src/app/model/model/ScheduleExam';
 
-
 @Component({
   selector: 'app-userexam',
   templateUrl: './userexam.component.html',
-  styleUrls: ['./userexam.component.css']
+  styleUrls: ['./userexam.component.css'],
 })
 export class UserexamComponent {
-
   code?: any;
-  currentQuestion?: Question ;
-  subjectId?:number;
-  uid:any;
-  eid:any;
+  currentQuestion?: Question;
+  subjectId?: number;
+  uid: any;
+  eid: any;
   questions: Question[] = [];
-  subjects: Subject[] =[];
+  subjects: Subject[] = [];
   selectedOptions: string[] = [];
-  answer:useranswer=new useranswer() ;
-  uniqueSubjectNames: String[] =[];
-  selected:boolean=false;
 
-
+  answer: useranswer = new useranswer();
+  uniqueSubjectNames: String[] = [];
+  selected: boolean = false;
   stateChange:number[]=[];
   questionnumber:number=0;
   subjectnumber:number=0;
-  constructor(private http: HttpClient,private route:ActivatedRoute,private service:MyserviceService,private router:Router) {}
-
-
+  constructor(
+    private http: HttpClient,
+    private route: ActivatedRoute,
+    private service: MyserviceService,
+    private router: Router
+  ) {}
 
 
 
   ngOnInit(): void {
 
-    this.uid=this.service.sendid();
-    this.eid=this.service.sendeid();
+    this.uid = this.service.sendid();
+    this.eid = this.service.sendeid();
+    console.log(this.uid, this.eid);
+    this.route.params.subscribe((params) => {
 
-    console.log(this.uid,this.eid)
-    this.route.params.subscribe(params => {
       this.code = params['code'];
       console.log('Exam code:', this.code);
       this.http.get<Question[]>(`http://localhost:8089/api/getquestionsBySubjectId/${this.code}`).subscribe(data=>this.questions=data);
       this.loadSubjects().subscribe((subjects: Subject[]) => {
-          this.subjects = subjects;
-          this.uniqueSubjectNames = this.getUniqueSubjectNames(this.subjects)
-          //console.log(this.questions)
-        });
+        this.subjects = subjects;
+        this.uniqueSubjectNames = this.getUniqueSubjectNames(this.subjects);
+        //console.log(this.questions)
+      });
     });
   }
 
@@ -64,8 +64,13 @@ export class UserexamComponent {
   }
 
   loadSubjects(): Observable<Subject[]> {
-    return this.http.post<Subject[]>(`http://localhost:8089/api/getsubjectsBycode/${this.code}`,this.code);
+    return this.http.post<Subject[]>(
+      `http://localhost:8089/api/getsubjectsBycode/${this.code}`,
+      this.code
+    );
   }
+
+
 
   getQuestionsBySubjectName(subjectName:String):void{
     this.questions=[];
@@ -77,20 +82,23 @@ export class UserexamComponent {
                     this.questionnumber=0
                   })
                   }})
+
   }
 
-  loadQuestions(subjectid?:number): Observable<Question[]> {
-    return this.http.get<Question[]>(`http://localhost:8089/api/getquestionsBySubjectId/${subjectid}/${this.code}`);
+  loadQuestions(subjectid?: number): Observable<Question[]> {
+    return this.http.get<Question[]>(
+      `http://localhost:8089/api/getquestionsBySubjectId/${subjectid}/${this.code}`
+    );
   }
 
   showQuestion(questionId: number): void {
-    this.questions.forEach((question)=>{
-                            if(question.id==questionId){
-                              this.currentQuestion=question;
-                            }
-    })
-
+    this.questions.forEach((question) => {
+      if (question.id == questionId) {
+        this.currentQuestion = question;
+      }
+    });
   }
+
 
   sendoption(qid:number,option1:string)
   {
@@ -98,34 +106,36 @@ export class UserexamComponent {
     console.log("Kutariya");
     this.stateChange.push(qid);
     console.log(this.stateChange);
+
     this.selectedOptions[qid] = option1;
-    console.log(option1)
-  this.answer = {
-    user: {
-      id: this.uid
-    },
-    exam: {
-      id: this.eid
-    },
-    question: {
-      id: qid
-    },
-     userAnswer:option1,
-  };
+    console.log(option1);
+    this.answer = {
+      user: {
+        id: this.uid,
+      },
+      exam: {
+        id: this.eid,
+      },
+      question: {
+        id: qid,
+      },
+      userAnswer: option1,
+    };
 
-console.log(this.answer.user?.id+"uid")
-console.log(this.answer.exam?.id+"eid")
-console.log(this.answer.question?.id+"qid")
-console.log(this.answer.userAnswer)
+    console.log(this.answer.user?.id + 'uid');
+    console.log(this.answer.exam?.id + 'eid');
+    console.log(this.answer.question?.id + 'qid');
+    console.log(this.answer.userAnswer);
 
-
-    console.log("+++++++++++++++++++++++++")
-   console.log(this.answer.userAnswer);
-       this.http.post(`http://localhost:8089/api/saveanswer`,this.answer).subscribe(data=>console.log(data));
-
-
+    console.log('+++++++++++++++++++++++++');
+    console.log(this.answer.userAnswer);
+    this.http
+      .post(`http://localhost:8089/api/saveanswer`, this.answer)
+      .subscribe((data) => console.log(data));
   }
   isOptionSelected(questionId: number, option: string): boolean {
+
+
     return this.selectedOptions[questionId] ===option;
   }
 clickEvent(exam: any) {

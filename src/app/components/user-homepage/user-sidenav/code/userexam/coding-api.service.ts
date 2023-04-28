207a1a99fce954a1fb97026352169b557863b71a
Suresh-130 <129723623+Suresh-130@users.noreply.github.com>
2023-04-28 18:18:57 +0530

    Coding (#62)

--- src/app/components/user-homepage/user-sidenav/code/userexam/coding-api.service.ts
+++ src/app/components/user-homepage/user-sidenav/code/userexam/coding-api.service.ts
@@ -1,3 +1,4 @@
+import { UserCode } from './../../../../../model/model/UserCode';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import * as qs from 'qs';
@@ -8,10 +9,14 @@ import { Observable } from 'rxjs';
 })
 export class CodingApiService {
 
+  userCode?:UserCode
+
   constructor(private http : HttpClient) { }
 
   runCode(code: string, language: string): Observable<any> {
     const url = 'https://api.codex.jaagrav.in';
+
+    const javaUrl='http://localhost:8089/api/savecode'
     const headers = new HttpHeaders({
       'Content-Type': 'application/x-www-form-urlencoded'
     });
@@ -20,7 +25,39 @@ export class CodingApiService {
       'language': language
       // 'input': input
     };
+    const headersInJava = new HttpHeaders({
+      'Content-Type': 'application/octet-stream'
+    });
+   // const headersInJava = { 'Content-Type': 'application/octet-stream' };
 
+
+// //     const text = "Hello, world!";
+// const  encoder = new TextEncoder();
+//  const bytes = encoder.encode(code);
+// const userCode: { code?: Uint8Array } = { code: bytes };
+
+
+    this.userCode={
+      language:'java',
+      userInputCode:code,
+      exam:{
+        id:1
+      },
+      user:{
+        id:2
+      },
+      codingQuestion:{
+        id:1
+      }
+
+    }
+      this.http.post(javaUrl,this.userCode).subscribe(response => {
+        console.log('Upload successful!');
+      }, error => {
+        console.error('Upload failed:', error);
+      });
+
+      
     return this.http.post(url, qs.stringify(data), { headers: headers });
   }
 

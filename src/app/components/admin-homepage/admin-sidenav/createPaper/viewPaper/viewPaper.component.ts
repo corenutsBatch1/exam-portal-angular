f44fb4ff93570aa6d38f01f9df1ac78947d01b31
Suresh-130 <129723623+Suresh-130@users.noreply.github.com>
2023-05-01 14:30:05 +0530

    viewCoding (#81)

--- src/app/components/admin-homepage/admin-sidenav/createPaper/viewPaper/viewPaper.component.ts
+++ src/app/components/admin-homepage/admin-sidenav/createPaper/viewPaper/viewPaper.component.ts
@@ -1,7 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+
 import { CreatePaper } from 'src/app/model/model/CreatePaper';
 import { Question } from 'src/app/model/model/Question';
+import { Subject } from 'src/app/model/model/Subject';
 
 @Component({
   selector: 'app-viewPaper',
@@ -10,7 +12,10 @@ import { Question } from 'src/app/model/model/Question';
 })
 export class ViewPaperComponent implements OnInit {
 questions?:number[]
+
 questionsArray:Question[]=[];
+
+
 @Output ('viewPaper')viewPaper =new EventEmitter();
 @Input() paperid?:any;
 paper:CreatePaper=new CreatePaper();
@@ -20,14 +25,19 @@ paper:CreatePaper=new CreatePaper();
     this.paper.id=this.paperid;
     console.log(this.paperid)
     console.log(this.paper.id)
+
     this.viewPaperQuestions(this.paper);
   }
+
   goBack(){
   this.viewPaper.emit(true);
   }
+
+
+
   viewPaperQuestions(paper2:CreatePaper){
-this.http.post<Question[]>(`http://localhost:8089/api/questionsbypaper`,paper2).subscribe(data=>{
-  console.log(paper2.id)
+  this.http.post(`http://localhost:8089/api/questionsbypaper`,paper2).subscribe(data=>{
+  console.log(data)
   this.questionsArray=this.questionsArray.concat(data);
 })
   }

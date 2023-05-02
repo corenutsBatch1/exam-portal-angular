648ac2d97c2674cee80e98626afecbf8e00c68aa
sambanna07 <115611654+sambanna07@users.noreply.github.com>
2023-05-03 00:46:25 +0530

    resetpasswordandconfirmpassword

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -14,84 +14,100 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 export class SignupComponent {
   hide=true;
-  signUpForm:any=FormGroup;
+  hidec=true;
+  signUpForm:any= FormGroup;
   submitted = false;
 
-  data:any | undefined;
-  userLogin:User = new User();
-  constructor(private http:HttpClient, private router : Router,
-    private formBuilder:FormBuilder){}
+  data: any | undefined;
+  userLogin: User = new User();
 
-  get f()
-  {
-   return this.signUpForm.controls;
-  }
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private formBuilder: FormBuilder,
+  ) {}
 
-ngOnInit(){
-  //Add User form validations
-  this.signUpForm = this.formBuilder.group({
-  sname:
-  [
-    '',
-   [
-    Validators.required,
-    Validators.minLength(3),
-    Validators.maxLength(30),
-    Validators.pattern('[a-zA-Z ]*')
-   ]
- ],
-  semail:
-  [
-    '',
-    [
-      Validators.required,
-      Validators.email,
-      Validators.pattern( /^\S*$/),
-    ]
-  ],
-  spassword:
-  [
-    '',
-    [
-      Validators.required,
-      Validators.minLength(6),
-      Validators.pattern( /^\S*$/),
-    ]
-  ],
-  sphoneNumber:
-  [
-    '',
-    [
-      Validators.required,
-      Validators.pattern('^[6-9][0-9]{9}$'),
-      // Validators.minLength(10),
-      // Validators.maxLength(10)
-    ]
-  ],
-  });
-}
-
-
-
-registerNewUser(newUser:User){
-this.submitted = true;
-console.log(newUser);
-if (this.signUpForm.invalid) {
-  console.log("false");
-  return;
-}
-if(this.submitted)
-{
-    this.http.post(`http://localhost:8088/api/registerUser`, newUser).subscribe(data=> {
-      if(data !=null){
-        swal("Registered successfully", 'User Name is '+this.userLogin.email, "success");
-        this.router.navigateByUrl('/login');
-
-      }
-      else{
-        swal("Already have an Account", "", "error");
-      }
+  ngOnInit() {
+    // Add User form validations
+    this.signUpForm = this.formBuilder.group({
+      sname: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(30),
+          Validators.pattern('[a-zA-Z ]*')
+        ]
+      ],
+      semail: [
+        '',
+        [
+          Validators.required,
+          Validators.email,
+          Validators.pattern( /^\S*$/),
+        ]
+      ],
+      spassword: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.pattern( /^\S*$/),
+        ]
+      ],
+      scpassword: [
+        '',
+        [
+          Validators.required,
+        ]
+      ],
+      sphoneNumber: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('^[6-9][0-9]{9}$'),
+          // Validators.minLength(10),
+          // Validators.maxLength(10)
+        ]
+      ],
+    }, {
+      validator: this.passwordMatchValidator('spassword', 'scpassword') // Add custom validator
     });
   }
-}
+
+  // Custom validator function to check if password and confirm password match
+  passwordMatchValidator(passwordField: string, confirmPasswordField: string) {
+    return (group: FormGroup) => {
+      let password = group.controls[passwordField];
+      let confirmPassword = group.controls[confirmPasswordField];
+
+      if (password.value !== confirmPassword.value) {
+        confirmPassword.setErrors({ passwordMismatch: true });
+      }
+    };
+  }
+
+  // Getter for easy access to form fields in template
+  get f() {
+    return this.signUpForm.controls;
+  }
+
+  registerNewUser(newUser: User) {
+    this.submitted = true;
+    console.log(newUser);
+    if (this.signUpForm.invalid) {
+      console.log("false");
+      return;
+    }
+    if (this.submitted) {
+      this.http.post(`http://localhost:8088/api/registerUser`, newUser).subscribe(data => {
+        if (data != null) {
+          swal("Registered successfully", 'User Name is ' + this.userLogin.email, "success");
+          this.router.navigateByUrl('/login');
+        } else {
+          swal("Already have an Account", "", "error");
+        }
+      });
+    }
+  }
 }

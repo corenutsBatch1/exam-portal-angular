99e4d945ff9ac860a484ed50d003411546b82222
Suresh-130 <suresh.kuruva14335@gmail.com>
2023-05-04 13:04:29 +0530

    statusChanges

--- src/app/components/admin-homepage/admin-sidenav/schedule-exam/schedule-exam.component.ts
+++ src/app/components/admin-homepage/admin-sidenav/schedule-exam/schedule-exam.component.ts
@@ -1,3 +1,67 @@
+// import { HttpClient } from '@angular/common/http';
+// import { ScheduleExam } from './../../../../model/model/ScheduleExam';
+// import { Component, OnInit } from '@angular/core';
+// import swal from 'sweetalert';
+// @Component({
+//   selector: 'app-schedule-exam',
+//   templateUrl: './schedule-exam.component.html',
+//   styleUrls: ['./schedule-exam.component.css']
+// })
+// export class ScheduleExamComponent implements OnInit {
+
+//   scheduleExam:ScheduleExam=new ScheduleExam();
+//   exams:ScheduleExam[]=[];
+//   addexam:boolean=true;
+//   constructor(private http:HttpClient) { }
+
+//   ngOnInit() {
+
+//    this.fetchExam();
+//   }
+//   loadAddExampage(flag:boolean){
+
+//     this.addexam=flag;
+//   }
+
+//   fetchExam(){
+//     this.http.get<ScheduleExam[]>(`http://localhost:8089/api/getallexams`).subscribe(data=>{
+//     this.exams=data;
+//   });
+//   }
+
+//   deleteexam(id:any){
+//         this.http.delete(`http://localhost:8089/api/deleteExam/${id}`).subscribe(data=>{
+//         //   console.log(data);
+//         //   this.ngOnInit();
+//         // })
+//   }
+//   deleteexam(id:any){
+
+//     swal({
+//       title: "Are you sure you want to Delete? ",
+//       icon: "warning",
+//       buttons: ['Cancel', 'Yes, Delete'],
+//       dangerMode: true,
+//     })
+//     .then((deleteConfirmed: any) => {
+//       if (deleteConfirmed) {
+//         this.deleteexam(id,id2).subscribe(
+//       reponse=>{
+//         console.log(reponse);
+//         swal("Deleted successfully", '', "success");
+//         console.log(id);
+//         console.log(id2);
+//         this.ngOnInit();
+//       }
+//       );
+//       } else {
+//       }
+//        });
+
+//     }
+
+// }
+
 import { HttpClient } from '@angular/common/http';
 import { ScheduleExam } from './../../../../model/model/ScheduleExam';
 import { Component, OnInit } from '@angular/core';
@@ -28,16 +92,15 @@ export class ScheduleExamComponent implements OnInit {
     this.exams=data;
   });
   }
-
   deleteexam(id:any){
-        this.http.delete(`http://localhost:8089/api/deleteExam/${id}`).subscribe(data=>{
-
+       return this.http.delete(`http://localhost:8089/api/deleteExam/${id}`);
+        // .subscribe(data=>{
 
         //   console.log(data);
         //   this.ngOnInit();
         // })
   }
-  delete(id:any,id2:any){
+  delete(id:any){
 
     swal({
       title: "Are you sure you want to Delete? ",
@@ -47,12 +110,12 @@ export class ScheduleExamComponent implements OnInit {
     })
     .then((deleteConfirmed: any) => {
       if (deleteConfirmed) {
-        this.deleteexam(id,id2).subscribe(
+        this.deleteexam(id).subscribe(
       reponse=>{
         console.log(reponse);
         swal("Deleted successfully", '', "success");
         console.log(id);
-        console.log(id2);
+       
         this.ngOnInit();
       }
       );

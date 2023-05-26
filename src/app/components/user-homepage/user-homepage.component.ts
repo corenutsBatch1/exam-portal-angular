b093fdc57a9904e64e3f4f386be54023c1e0dd8d
Fahath Shaik <shaikfahath123@gmail.com>
2023-05-26 18:25:36 +0530

    fixed-fullscreen

--- src/app/components/user-homepage/user-homepage.component.ts
+++ src/app/components/user-homepage/user-homepage.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, HostListener, Inject, OnInit } from '@angular/core';
 import {  ActivatedRoute } from '@angular/router';
 // import { User } from 'src/app/model/model/User';
 
@@ -9,11 +10,31 @@ import {  ActivatedRoute } from '@angular/router';
 })
 export class UserHomepageComponent implements OnInit {
 id1?:number;
-  constructor(private route:ActivatedRoute) { }
+elem: any;
+  constructor(@Inject(DOCUMENT) private document: any, private route:ActivatedRoute) {
+
+  }
 
   ngOnInit() {
     this.id1=this.route.snapshot.params['id'];
     console.log(this.id1);
+    this.elem = document.documentElement;
+    this.openFullscreen()
+  }
+
+  openFullscreen() {
+    if (this.elem.requestFullscreen) {
+      this.elem.requestFullscreen();
+    } else if (this.elem.mozRequestFullScreen) {
+      /* Firefox */
+      this.elem.mozRequestFullScreen();
+    } else if (this.elem.webkitRequestFullscreen) {
+      /* Chrome, Safari and Opera */
+      this.elem.webkitRequestFullscreen();
+    } else if (this.elem.msRequestFullscreen) {
+      /* IE/Edge */
+      this.elem.msRequestFullscreen();
+    }
   }
 
 

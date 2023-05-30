2086ca98b4e4dc17a2349ac87c6f0f538fab2b90
Fahath Shaik <129723395+FahathShaik@users.noreply.github.com>
2023-05-30 15:29:56 +0530

    fixed-excel-sheet

--- src/app/components/admin-homepage/admin-sidenav/question-bank/question-bank.component.ts
+++ src/app/components/admin-homepage/admin-sidenav/question-bank/question-bank.component.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { saveAs } from 'file-saver';
+import { Subject } from 'src/app/model/model/Subject';
+import { SubjectService } from 'src/app/services/subject.service';
 import * as XLSX from 'xlsx';
 
 @Component({
@@ -10,26 +12,28 @@ import * as XLSX from 'xlsx';
   styleUrls: ['./question-bank.component.css']
 })
 export class QuestionBankComponent implements OnInit {
-  showQuestion:boolean=true;
-  manageQuestions:boolean=true;
-  showCodingQuestions:boolean=true;
-  constructor(private route:Router,private http : HttpClient) { }
+  showQuestion: boolean = true;
+  manageQuestions: boolean = true;
+  showCodingQuestions: boolean = true;
+  subjects?: Subject[];
+  constructor(private route: Router, private http: HttpClient, private subjectService: SubjectService) { }
 
   ngOnInit() {
-
-
+    this.subjectService.fetchSubjects().subscribe((data) => {
+      this.subjects = data;
+    });
   }
-  loadAddQuestionPage(flag:boolean){
+  loadAddQuestionPage(flag: boolean) {
     // alert("route ")
-    this.showQuestion=flag;
+    this.showQuestion = flag;
   }
-  loadManageQuestionPage(flag:boolean){
+  loadManageQuestionPage(flag: boolean) {
     // alert("route ")
-    this.manageQuestions=flag;
+    this.manageQuestions = flag;
   }
 
-  loadAddCodingQuestionPage(flag:boolean){
-    this.showCodingQuestions=flag;
+  loadAddCodingQuestionPage(flag: boolean) {
+    this.showCodingQuestions = flag;
   }
 
 
@@ -40,11 +44,11 @@ export class QuestionBankComponent implements OnInit {
     this.sendFileToServer(file)
   }
 
-  sendFileToServer(file : File){
+  sendFileToServer(file: File) {
     const formData = new FormData();
     formData.append('file', file);
 
-    this.http.post('http://localhost:9033/api/questions/upload',formData).subscribe(response => {
+    this.http.post('http://localhost:9033/api/questions/upload', formData).subscribe(response => {
       console.log("File uploaded successfully")
     });
   }
@@ -52,15 +56,14 @@ export class QuestionBankComponent implements OnInit {
 
   generateExcelFile() {
     const worksheet: XLSX.WorkSheet = XLSX.utils.aoa_to_sheet([
-      ['Column 1', 'Column 2', 'Column 3', 'Column 4', 'Column 5', 'Column 6', 'Column 7'],
-      ['', '', '', '', '', '', ''], // Add empty row
-    ]);
-
+      ['subject', 'topic', 'answer', 'content', 'option_a', 'option_b', 'option_c', 'option_d', 'q_type'],
+      ['', '', '', '', '', '', '', '', ''], // Add empty row
+    ]);  
     const workbook: XLSX.WorkBook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
-
+  
     const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
     const data: Blob = new Blob([excelBuffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8' });
     saveAs(data, 'excel_file.xlsx');
-}
+  }
 
 }

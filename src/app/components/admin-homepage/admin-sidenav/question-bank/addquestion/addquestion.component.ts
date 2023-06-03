fcced354c99fd8882062c0d0a317c99a47707c2f
Thirumalesh-63 <129723705+Thirumalesh-63@users.noreply.github.com>
2023-06-03 11:25:14 +0530

    empty (#150)

--- src/app/components/admin-homepage/admin-sidenav/question-bank/addquestion/addquestion.component.ts
+++ src/app/components/admin-homepage/admin-sidenav/question-bank/addquestion/addquestion.component.ts
@@ -26,6 +26,10 @@ export class AddquestionComponent implements OnInit{
    subjects?:Subject[];
    uniqueSubjectNames: string[] = [];
    answers : string[] = []
+    isCheckedA:boolean=false;
+    isCheckedB:boolean=false;
+    isCheckedC:boolean=false;
+    isCheckedD:boolean=false;
    public Editor = ClassicEditor;
 
   constructor(private http:HttpClient,private router:Router,private subjectService : SubjectService)
@@ -70,6 +74,25 @@ export class AddquestionComponent implements OnInit{
      // this.filteredTopics = undefined;
     }
   }
+// Inside your component code
+
+// Function to clear the form fields
+clearFormFields() {
+  this.Questions = { content: '', optionA: '', optionB: '', optionC: '', optionD: '', answer: '', qtype: '' };
+  this.isCheckedA=false;
+  this.isCheckedB=false;
+  this.isCheckedC=false;
+  this.isCheckedD=false;
+
+  this.subject_id = undefined;
+  this.selectedsubject = ''; // Restore the selected subject
+  this.filteredTopics = []; // Update the filtered topics based on the selected subject
+}
+
+// Function to handle the "Clear" button click event
+onClearClick() {
+  this.clearFormFields();
+}
 
   addQuestion(Questions:Question,id?:number){
     this.answers.sort();
@@ -78,6 +101,13 @@ export class AddquestionComponent implements OnInit{
       this.http.post(`http://localhost:9033/api/addquestion/${id}`, Questions).subscribe(
         response=>{
           Swal.fire("Question added successfully","", "success");
+
+          this.Questions = { content: '', optionA: '', optionB: '', optionC: '', optionD: '', answer: '', qtype: '' };
+          this.isCheckedA=false;
+          this.isCheckedB=false;
+          this.isCheckedC=false;
+          this.isCheckedD=false;
+
         },
         error=>{
           Swal.fire("All field must be required","", "error");
@@ -90,7 +120,18 @@ export class AddquestionComponent implements OnInit{
   }
 
   checkboxChanged(event: any, optionValue: string) {
-    console.log(event.checked)
+    // console.log(event.checked)
+    if(optionValue == 'A'){
+      this.isCheckedA=true;
+    }
+    if(optionValue == 'B'){
+      this.isCheckedB=true;
+    } if(optionValue == 'C'){
+      this.isCheckedC=true;
+    } if(optionValue == 'D'){
+      this.isCheckedD=true;
+    }
+    console.log(this.isCheckedA);
     if (event.checked) {
       // Checkbox is checked
       this.answers.push(optionValue);
